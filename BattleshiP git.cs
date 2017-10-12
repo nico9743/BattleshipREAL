304449402bd0e59ea2c3d3c97345db210d6a8c81
nico9743 <32506660+nico9743@users.noreply.github.com>
2017-10-12 14:08:02 +0200

    Add files via upload

---
+++ BattleshiP git.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace spil
+{
+    public class BoardSlidesMenu
+    {
+        string mellem = "";
+        BoardSlides boardSlides { get; set; }
+        public void Show()
+        {
+
+
+            boardSlides = new BoardSlides();
+            boardSlides.SingleUse();
+            Console.Clear();
+
+            //var startTimeSpan = TimeSpan.Zero;
+            //var periodTimeSpan = TimeSpan.FromSeconds(1);
+
+            //var timer = new System.Threading.Timer((e) =>
+            //{
+
+            //}, null, startTimeSpan, periodTimeSpan);
+
+
+
+
+            bool running = true;
+            string choice = "";
+            do
+            {
+                Showmenu1();
+                choice = GetUserChoice();
+                switch (choice)
+                {
+                    case "1": DoActionFor1(); break;
+                    case "2": DoActionFor2(); break;
+                    case "3": DoActionFor3(); break;
+                    //case "q": MoveShip(); break;
+                    //case "4": DoActionFor4(); break;
+                    case "0": running = false; break;
+                    default: ShowMenuSelectionErroe(); break;
+                }
+            } while (running);
+        }
+
+        public void Showmenu1()
+        {
+
+            Console.Clear();
+            if (boardSlides != null)
+            {
+                Console.WriteLine(boardSlides.BoardSlideMap());
+
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\t BatteShip!!!!!!!!");
+            Console.WriteLine();
+            Console.WriteLine("\t 1. Opret nyt spil");
+            Console.WriteLine("\t 2. placer skib");
+            Console.WriteLine("\t 3. Flyt en brik");
+            Console.WriteLine();
+            Console.WriteLine("0. exit");
+            //bool tid = true;
+
+
+ //           Console.WriteLine("\n" +
+ //mellem + "                                     # #  ( ) \n" +
+ //mellem + "                                  ___#_#___|__\n" +
+ //mellem + "                              _  |____________|  _\n" +
+ //mellem + "                      _ =====| | |            | | |====_\n" +
+ //mellem + "                =====| |.---------------------------. | |====\n" +
+ //mellem + "   \\--------------------'   .  .  .  .  .  .  .  .   '-------------->\n" +
+ //mellem + "    \\                                                             /\n" +
+ //mellem + "     \\___________________________________________________________/\n" +
+ //mellem + " wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww\n" +
+ //mellem + "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww\n" +
+ //mellem + "  wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww\n");
+
+           // Console.ReadLine();
+            //while (tid == true)
+            //{
+            //    Thread.Sleep(1 * 2 * 1);
+            //    mellem += " ";
+
+            //}
+            //if (boardSlides != null)
+            //{
+            //    Console.WriteLine(boardSlides.BoardSlideMap());
+
+            //}
+
+
+        }
+        private string GetUserChoice()
+        {
+            Console.WriteLine();
+            Console.Write("Vælg bare bro\n");
+            return Console.ReadLine();
+        }
+        public void ShowMenuSelectionErroe()
+        {
+            Console.WriteLine("Nej det kan du ikke forhelvede.");
+            Console.ReadLine();
+        }
+        public void DoActionFor1()
+        {
+            boardSlides = new BoardSlides();
+            boardSlides.SingleUse();
+        }
+        public void DoActionFor2()
+        {
+            
+            String[] SkibNavne = { "Hangarskib", "Slagskib", "Destroyer", "Ubåd", "Patruljebåd" };
+
+            Console.WriteLine(
+                 
+                "\n 1. Hangarskib   - " + boardSlides.Hangarskib +
+                "\n 2. Slagskib     - " + boardSlides.slagskib +
+                "\n 3. Destroyer    - " + boardSlides.Destroyer +
+                "\n 4. Ubåd         - " +  boardSlides.ubåd +
+                "\n 5. PatruljeBåd  - " + boardSlides.patruljebåd);
+          
+            int type = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine
+                    (@"
+                    1. Down
+                    2. Up
+                    3. Left
+                    4. Right"
+                    );
+            int direction = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Indtast koordinat for "+ SkibNavne[type-1]);
+            string inputs = Console.ReadLine();
+            boardSlides.placeShip(inputs, type, direction);
+            
+        } 
+        public void DoActionFor3()
+        {
+
+        }
+        //private static System.Timers.Timer aTimer;
+        //public void  MoveShip(){
+        //    //while (true)
+        //    //{
+        //    //    mellem += " ";
+        //    //}
+        //mellem += " ";
+        //for (int i = 0; i < 100; i++)
+        //    mellem += " ";
+        //    aTimer = new System.Timers.Timer(100);
+
+        //    // Hook up the Elapsed event for the 
+
+        //timer.
+        //    aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+
+        //    // Set the Interval to 2 seconds (2000 milliseconds).
+        //    aTimer.Interval = 100;
+        //    aTimer.Enabled = true;
+
+
+        //}
+
+        //private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        //{
+        //    mellem += " ";
+        //}
+
+        public void ValidateWinner()
+        {
+
+        }
+    } 
+}
---
+++ BattleshipBackupLast.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spil
+{
+    public class BoardSlides
+    {
+        public char[,] GameBoard { get; set; }
+        public char[,] GameBoard2 { get; set; }
+        public string l = "";
+        public string k = "";
+        public void SingleUse()        {
+            GameBoard = new char[10, 10];
+            GameBoard2 = new char[10, 10];
+            for (int i = 0; i < 10; 
+                i++)
+            {
+                for (int j = 0; j < 10; j++)
+
+                {
+                    GameBoard[i, j] = ' ';
+                    GameBoard2[i, j] = ' ';
+                }
+                
+            }
+        }
+        public string BoardSlideMap()
+        {            string result = "  | 1 | 2 | 3 | 4 | 5| 6 | 7 | 8 | 9 | 10 |\n\n";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+            for (int i = 0; i < 10; i++)
+            {
+                if (i < 9)
+                {
+                    result += (i + 1) + " | ";
+                }
+
+                else
+                {
+                    result += (i + 1) + "| ";
+                }
+                    
+                    for (int j = 0; j < 10; j++)
+                {
+                    result += GameBoard[i, j] +" | ";
+                    }
+                    result += "\n" + "-------------------------------------------";
+
+                    result += "\n";
+                
+
+                
+
+            }
+            result = result + l;
+            
+
+            return result ;
+            //BoardSlidesMenu boardSlidesMenu = new BoardSlidesMenu(); 
+            
+            
+        }
+        int shipPlayer = 0;
+        public int Destroyer = 2;
+        public int Hangarskib = 1;
+        public int patruljebåd = 3;
+        public int slagskib = 2;
+        public int ubåd = 1;
+        public void placeShip(string input, int Skib, int direction)
+        {
+            int[] skibsLængde = { 5, 4, 3, 3, 2 };
+            char[] skibForbogstav = { 'H', 'S', 'D', 'U', 'P' }; 
+            int valgteSkib = skibsLængde[Skib - 1];
+            //Console.ForegroundColor = ConsoleColor.Green;            
+            string[] placeP = input.Split(',');
+            int x = Convert.ToInt32(placeP[0]) - 1;
+            int y = Convert.ToInt32(placeP[1]) - 1;
+            l = "" + x + "" + y;
+            
+            
+            if (shipPlayer == 0)
+            {
+                
+                shipPlayer = 1;
+            }
+            else
+            {
+                shipPlayer = 0;
+            }
+
+            int isPlaceAvailable = 0;
+            // gemme værdier I gameboardet!
+
+
+            for (int j = 0; j < valgteSkib; j++)
+            {
+                if (direction == 1 && (y + j) >= 0 && (y + j) <= 9 && x >= 0 && x <= 9)
+                {
+                    if (GameBoard[y + j, x] == ' ')
+                        isPlaceAvailable++;
+                }
+                if (direction == 2 && (y - j) >= 0 && (y - j) <= 9 && x >= 0 && x <= 9)
+                {
+                    if (GameBoard[y - j, x] == ' ')
+                        isPlaceAvailable++;
+                }
+                if (direction == 3 && (x - j) >= 0 && (x - j) <= 9 && y>= 0 && y<= 9) 
+                {
+                    if (GameBoard[y, x - j] == ' ')
+                        isPlaceAvailable++;
+                }
+                if (direction == 4 && (x + j) >= 0 && (x + j) <= 9 && y >= 0 && y <= 9)
+                {
+                    if (GameBoard[y, x + j] == ' ')
+                        isPlaceAvailable++;
+                }
+            }
+
+
+            if (isPlaceAvailable == valgteSkib)
+            {
+                if (Skib == 1)
+                    Hangarskib--;
+                if (Skib == 2)
+                    slagskib--;
+                if (Skib == 3)
+                    Destroyer--;
+                if (Skib == 4)
+                    ubåd--;
+                if (Skib == 5)
+                    patruljebåd--;
+
+
+                for (int i = 0; i < valgteSkib; i++)
+                {
+                    if (direction == 4)
+                        GameBoard[y, x + i] = skibForbogstav[Skib - 1];
+                    if (direction == 2)
+                        GameBoard[y - i, x] = skibForbogstav[Skib - 1];
+                    if (direction == 3)
+                        GameBoard[y, x - i] = skibForbogstav[Skib - 1];
+                    if (direction == 1)
+                        GameBoard[y + i, x] = skibForbogstav[Skib - 1];
+                }
+            }
+            else
+            {
+                BoardSlidesMenu menu = new BoardSlidesMenu();
+                menu.ShowMenuSelectionErroe();
+            }
+            if (Hangarskib ==0 && slagskib == 0 && Destroyer == 0 && ubåd == 0 && patruljebåd == 0)
+            {
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
